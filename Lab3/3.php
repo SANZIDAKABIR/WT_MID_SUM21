583cb6a27a03668d4df8f6771cfa5a2e4cdd8be2
Sanzida <sanzida@gmail.com>
2021-06-17 13:43:33 +0600

    commited by tnavir

---
+++ Lab3/3.php
@@ -0,0 +1,14 @@
+<?php
+$length=5;
+$width=5;
+
+$perimeter= 2*($length+$width);
+echo "Perimeter: $perimeter<br>";
+
+$area= ($length*$width);
+echo "Area: $area<br>";
+
+if($length===$width){
+echo "the shape is a square";
+}
+?>
